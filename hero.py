09a0f03da87d91776068a666fa2768f6597ae67e
fadhlu <fadhlurahman.i@gmail.com>
2020-05-19 14:12:27 +0700

    fix hero structure

--- hero.py
+++ hero.py
@@ -4,11 +4,20 @@ import re
 import os
 import shutil
 
+
 class Hero(object):
-    def __init__(self, hero):
-        self.hero = hero
-        self.hero_name = hero['data']['name']
+    def __init__(self, url):
+        self.hero_url = url
+        self.hero = self.create_json()
+        self.hero_name = self.hero['data']['name']
         self.path = f'./hero/{self.hero_name}'
+        self.hero_img_url = url.replace("detail", "atlas")
+
+    def create_json(self):
+        re = requests.get(self.hero_url)
+        hero_json = json.loads(re.text)
+
+        return hero_json
 
     def remove_necessary_char(self, text):
         result = re.sub("#", "", text)
@@ -16,21 +25,32 @@ class Hero(object):
 
         return result
 
-    def download_hero_img(self, url):
-        re = requests.get(url)
+    def download_hero_img(self):
+        re = requests.get(self.hero_img_url)
         hero_url = json.loads(re.text)
-        url = hero_url['data'][0]['picture']
+        url = hero_url['data']
 
-        re = requests.get(url, stream=True)
+        img_list = []
+        for i in range((len(url))):
+            img_list.append({'img_name': hero_url['data'][i]['title'],
+                             'url': hero_url['data'][i]['picture']})
 
-        with open(f"{self.path}/cover.png" , 'wb') as f:
-            re.raw.decode_content = True
-            shutil.copyfileobj(re.raw, f)
+        for img in img_list:
+            re = requests.get(img['url'], stream=True)
 
+            try:
+                with open(f"{self.path}/img/cover_{img['img_name']}.png", 'wb') as f:
+                    re.raw.decode_content = True
+                    shutil.copyfileobj(re.raw, f)
+            except:
+                os.makedirs(f"{self.path}/img")
+                with open(f"{self.path}/img/cover_{img['img_name']}.png", 'wb') as f:
+                    re.raw.decode_content = True
+                    shutil.copyfileobj(re.raw, f)
 
     def create_hero(self):
         story = self.remove_necessary_char(self.hero['data']['des'])
-        
+
         data = {
             'name': self.hero_name,
             'type': self.hero['data']['type'],
@@ -64,6 +84,7 @@ class Hero(object):
                 }
             }
         }
+
         try:
             with open(f'{self.path}/{self.hero_name}.json', 'w') as f:
                 json.dump(data, f, indent=4)
@@ -71,3 +92,5 @@ class Hero(object):
             os.makedirs(self.path)
             with open(f'{self.path}/{self.hero_name}.json', 'w') as f:
                 json.dump(data, f, indent=4)
+
+        self.download_hero_img()
--- scrape-hero.py
+++ scrape-hero.py
@@ -3,18 +3,12 @@ import requests
 import json
 from hero import Hero
 
-for i in range(10):
-    i += 1
+# for i in range(9):
+#     i += 1
 
-    url = f"https://mapi.mobilelegends.com/hero/detail?id={i}&language=en"
-    url_cover = f"https://mapi.mobilelegends.com/hero/atlas?id={i}&language=en"
+url = f"https://mapi.mobilelegends.com/hero/detail?id=2&language=en"
+# url_cover = f"https://mapi.mobilelegends.com/hero/atlas?id=2&language=en"
 
-    re = requests.get(url)
-    content = re
-
-    hero_json = json.loads(re.text)
-
-    hero = Hero(hero_json)
-    hero.create_hero()
-    hero.download_hero_img(url_cover)
+hero = Hero(url)
+hero.create_hero()
 
